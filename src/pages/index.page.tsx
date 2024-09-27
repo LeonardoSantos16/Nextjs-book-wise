11aa9c176f1f3e4a1e984ea2cba42a4a31c106fd
leonardo <leodsmf@gmail.com>
2024-09-27 15:17:25 -0300

    estrutura inicial do projeto

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,4 @@
+import { HelloColor } from "./styles"
+export default function Home(){
+  return <HelloColor>Hello world</HelloColor>
+}
---
+++ src/pages/styles.ts
@@ -0,0 +1,6 @@
+import { styled } from "../stitches.config";
+
+export const HelloColor = styled('div', {
+    color: '$green100',
+    fontSize: '$xs',
+})
